1aee56c0b47600311c7d839e31692b4290f44629
lastblues <bluebomber9103@hotmail.com>
2016-08-04 10:04:56 -0700

    update v0.03
    finished scrolling
    added logo picture
    made it the way I want it
    moving on to content

--- js/scroll.js
+++ js/scroll.js
@@ -2,8 +2,11 @@ var mn = $(".navbar");
 mns = "navbar-scrolled";
 hdr = $('header').height();
 
+/*IF THE CONTENT REACHES A CERTAIN HEIGHT (IN THIS CASE 300PX AFTER MUCH TESTING)
+	add the navbar-scrolled class which will add new attributes and fucntion
+	*/
 $(window).scroll(function(){
-	if( $(this).scrollTop() > 250){
+	if( $(this).scrollTop() > 300){
 		mn.addClass(mns);
 	}
 	else{
